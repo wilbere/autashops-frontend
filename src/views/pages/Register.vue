95dd8ee10585bfab46c989ee615cb8f329087f4c
Wilber Galindez <wilbergalindezm23@gmail.com>
2020-08-21 22:48:20 -0400

    Installation

---
+++ src/views/pages/Register.vue
@@ -0,0 +1,156 @@
+<!-- =========================================================================================
+    File Name: Login.vue
+    Description: Login Page
+    ----------------------------------------------------------------------------------------
+    Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
+      Author: Pixinvent
+    Author URL: http://www.themeforest.net/user/pixinvent
+========================================================================================== -->
+
+
+<template>
+  <div class="h-screen flex w-full bg-img vx-row no-gutter items-center justify-center" id="page-login">
+    <div class="vx-col sm:w-1/2 md:w-1/2 lg:w-3/4 xl:w-3/5 sm:m-0 m-4">
+      <vx-card>
+        <div slot="no-body" class="full-page-bg-color">
+
+          <div class="vx-row no-gutter justify-center items-center">
+
+            <div class="vx-col hidden lg:block lg:w-1/2">
+              <img src="@/assets/images/pages/register.jpg" alt="login" class="mx-auto">
+            </div>
+
+            <div class="vx-col sm:w-full md:w-full lg:w-1/2 d-theme-dark-bg">
+              <div class="p-8 login-tabs-container">
+
+                <div class="vx-card__title mb-4">
+                  <h4 class="mb-4">Login</h4>
+                  <p>Welcome back, please login to your account.</p>
+                </div>
+
+                <div>
+                  <vs-input
+                    v-validate="'required|alpha_dash|min:3'"
+                    data-vv-validate-on="blur"
+                    label-placeholder="Name"
+                    name="name"
+                    placeholder="Name"
+                    v-model="name"
+                    class="w-full" />
+                  <!-- <span class="text-danger text-sm">{{ errors.first('name') }}</span> -->
+
+                  <vs-input
+                    v-validate="'required|email'"
+                    data-vv-validate-on="blur"
+                    name="email"
+                    type="email"
+                    label-placeholder="Email"
+                    placeholder="Email"
+                    v-model="email"
+                    class="w-full mt-6" />
+                  <!-- <span class="text-danger text-sm">{{ errors.first('email') }}</span> -->
+
+                  <vs-input
+                    ref="password"
+                    type="password"
+                    data-vv-validate-on="blur"
+                    v-validate="'required|min:6|max:10'"
+                    name="password"
+                    label-placeholder="Password"
+                    placeholder="Password"
+                    v-model="password"
+                    class="w-full mt-6" />
+                  <!-- <span class="text-danger text-sm">{{ errors.first('password') }}</span> -->
+
+                  <vs-input
+                    type="password"
+                    v-validate="'min:6|max:10|confirmed:password'"
+                    data-vv-validate-on="blur"
+                    data-vv-as="password"
+                    name="confirm_password"
+                    label-placeholder="Confirm Password"
+                    placeholder="Confirm Password"
+                    v-model="confirm_password"
+                    class="w-full mt-6" />
+                  <!-- <span class="text-danger text-sm">{{ errors.first('confirm_password') }}</span> -->
+
+                  <vs-checkbox v-model="isTermsConditionAccepted" class="mt-6">I accept the terms & conditions.</vs-checkbox>
+                  <vs-button  type="border" to="/pages/login" class="mt-6">Login</vs-button>
+                  <vs-button class="float-right mt-6" @click="registerUser" :disabled="!validateForm">Register</vs-button>
+
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </vx-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      confirm_password: '',
+      isTermsConditionAccepted: true,
+    }
+  },
+  computed: {
+    validateForm () {
+      return  this.name !== '' && this.email !== '' && this.password !== '' && this.confirm_password !== '' && this.isTermsConditionAccepted === true
+    }
+  },
+  methods: {
+    checkLogin () {
+      // If user is already logged in notify
+      if (this.$store.state.auth.isUserLoggedIn()) {
+
+        // Close animation if passed as payload
+        // this.$vs.loading.close()
+
+        this.$vs.notify({
+          title: 'Login Attempt',
+          text: 'You are already logged in!',
+          iconPack: 'feather',
+          icon: 'icon-alert-circle',
+          color: 'warning'
+        })
+
+        return false
+      }
+      return true
+    },
+    registerUser () {
+      // If form is not validated or user is already login return
+      if (!this.validateForm || !this.checkLogin()) return
+
+      const payload = {
+        userDetails: {
+          displayName: this.displayName,
+          email: this.email,
+          password: this.password,
+          confirmPassword: this.confirm_password
+        },
+        notify: this.$vs.notify
+      }
+      this.$store.dispatch('auth/registerUser', payload)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#page-login {
+  .social-login-buttons {
+    .bg-facebook { background-color: #1551b1 }
+    .bg-twitter { background-color: #00aaff }
+    .bg-google { background-color: #4285F4 }
+    .bg-github { background-color: #333 }
+  }
+}
+</style>
